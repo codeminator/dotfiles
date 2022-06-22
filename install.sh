5243d28a745fa788d2c244149f32eb1aa4a9ce6f
Ahmed Shahin <codeminator@github.com>
2022-06-22 12:57:21 +0200

    Fix auto-suggestion/default shell

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo apt-get install -y rcm
 rcup -d . -f -v
+sudo chsh -s $(which zsh) vscode
